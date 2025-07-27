3b3f3da881eefeeb7590723eb8af43681164fa1c
yaser mohammad alshikh <yaseralshikh@hotmail.com>
2025-07-27 18:53:52 +0300

    feat: Integrate SweetAlert2 for success and error notifications, update package dependencies

--- resources/views/components/layouts/app/header.blade.php
+++ resources/views/components/layouts/app/header.blade.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}" class="dark">
     <head>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
         @include('partials.head')
     </head>
     <body class="min-h-screen bg-white dark:bg-zinc-800">
